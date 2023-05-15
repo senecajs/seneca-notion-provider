bd6910a2cb2fc2e5f50cf10ec7ecf7f0405a9c33
Alex Milenkovic <jk1143298@gmail.com>
2023-05-15 08:47:16 +0200

    basic messages

--- test/basic.messages.js
+++ test/basic.messages.js
@@ -5,14 +5,77 @@ module.exports = {
   pattern: 'sys:provider,provider:notion',
   allow: { missing: true },
 
+  data: {
+    notion: {
+      page: {
+        pg01: { id: 'pg01', }
+      },
+      database: {
+        db0: { id: 'db01' }
+      }
+    }
+  },
+
   calls: [
     {
-      pattern: 'get:info',
+      name: 'get_info',
+      pattern: 'get:info,provider:notion,sys:provider',
       out: {
         ok: true,
         name: 'notion',
         version: Pkg.version,
       },
+    },
+
+    {
+      name: 'load_page',
+      pattern: 'base:notion,cmd:load,name:page,role:entity',
+      params: {
+          id: 'pg01'
+      },
+      out: { 'entity$': '-/notion/page', id: 'pg01' }
+
+    },
+
+    {
+      name: 'load_database',
+      pattern: 'base:notion,cmd:load,name:database,role:entity',
+      params: {
+          id: 'db01'
+      },
+      out: { 'entity$': '-/notion/database', id: 'db01' }
+
+    },
+
+    {
+      name: 'list_page',
+      pattern: 'base:notion,cmd:list,name:page,role:entity',
+      params: {},
+      out: [ { 'entity$': '-/notion/page', id: 'pg01' } ],
+
+    },
+
+    {
+      name: 'list_database',
+      pattern: 'base:notion,cmd:list,name:database,role:entity',
+      params: {},
+      out: [ { 'entity$': '-/notion/database', id: 'db01' } ],
+
+    },
+
+    /*
+    {
+      name: 'save_page',
+      pattern: 'base:notion,cmd:save,name:page,role:entity',
+      params: {
+        page: {
+          id$: 'pg02',
+        },
+      },
+
+      out: {}
     }
+    */
+
   ]
 }
