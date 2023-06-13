c713050128b2a9156c19cb885566911d66e43585
Alex Milenkovic <jk1143298@gmail.com>
2023-06-13 18:31:43 +0200

    commit

--- src/NotionProvider-doc.ts
+++ src/NotionProvider-doc.ts
@@ -2,39 +2,52 @@
 
 const Joi = require('@hapi/joi')
 
-const docs = {
+const messages = {
   get_info: {
     desc: 'Get information about the Notion SDK.',
   },
 
   load_page: {
     desc: 'Load Notion page data into an entity.',
-    examples: {
-    },
-    reply_desc: {
-    }
+    examples: {},
+    reply_desc: {}
   },
 
   save_page: {
     desc: 'Update/Save Notion page data into an entity.',
-    examples: {
-    },
-    reply_desc: {
-    }
+    examples: {},
+    reply_desc: {}
   },
 
   list_page: {
     desc: 'List Noion page data into an entity.',
-    examples: {
-    },
-    reply_desc: {
-    }
+    examples: {},
+    reply_desc: {}
   }
 
 }
 
-export default docs
+const sections = {
+  /*
+  intro: {
+    path: '../seneca-provider/doc/intro.md'
+  },
+
+  support: {
+    path: '../seneca-provider/doc/support.md'
+  }
+  */
+
+}
+
+export default {
+  messages,
+  sections,
+}
 
 if ('undefined' !== typeof(module)) {
-  module.exports = docs
+  module.exports = {
+    messages,
+    sections,
+  }
 }
